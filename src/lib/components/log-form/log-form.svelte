9b1cf079155cd9359f188f19c02005a80979ce25
Robert Sable <robertyoung10101@gmail.com>
2024-12-22 17:07:25 +1300

    Add shadcn components for a simple form with a date picker

---
+++ src/lib/components/log-form/log-form.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import {getLocalTimeZone, today, type DateValue} from "@internationalized/date";
+	import Button from "../ui/button/button.svelte";
+	import * as Card from "../ui/card";
+	import DatePicker from "../ui/date-picker/date-picker.svelte";
+	import {Label} from "../ui/label";
+
+	let datePlayed: DateValue | undefined = $state(today(getLocalTimeZone()));
+</script>
+
+<Card.Root class="w-[400px]">
+	<Card.Header>
+		<Card.Title>Log game</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<form>
+			<div class="grid w-full items-center gap-4">
+				<div>
+					<Label for="date-picker">Date played</Label>
+					<DatePicker id="date-picker" bind:value={datePlayed} />
+				</div>
+
+				<Button>Submit</Button>
+			</div>
+		</form>
+		
+	</Card.Content>
+	
+</Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import LogForm from "$lib/components/log-form/log-form.svelte";
+
+</script>
+
+<LogForm/>
